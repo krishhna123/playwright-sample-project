f2929653b35a2ed167ef4e84430497c10fb5a22d
krishhna123 <krishhna123@gmail.com>
2026-08-02 11:27:59 +0200

    feat: fixing baseline generation in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  /* Auto-create missing snapshots instead of failing */
+  updateSnapshots: "missing",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
